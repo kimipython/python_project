8202c98e3ed6d361f2773bb8909690e112728f48
zuiaimamacaiYW <yao_wen2008816@163.com>
2018-05-26 14:20:33 +0800

    配置加载信息开启测试模式

--- 01-set app.py
+++ 01-set app.py
@@ -1,9 +1,20 @@
 #coding=utf-8
 
-
 from flask import Flask
 
+# 配置文件的加载
+
+
+class config(object):
+    # 开启测试模式
+    DEBUG = True
+
 app = Flask(__name__)
+
+# 在app里获取加载信息
+
+app.config.from_object(config)
+
 
 @app.route('/')
 def index():
